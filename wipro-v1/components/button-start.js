cef2c04f279be917a0115a35ec70a5f416715a26
fabmeyer <fabian@meyerfoto.ch>
2019-10-22 17:13:48 +0200

    added output box

--- wipro-v1/components/button-start.js
+++ wipro-v1/components/button-start.js
@@ -9,7 +9,23 @@ class ButtonStart extends React.Component {
       bitString: this.props.bitString,
       baseString: this.props.baseString
     };
+
+    this.checktype(this.props.strLength);
   }
+
+  checktype = arg => {
+    if (typeof arg !== "number") {
+      console.log(arg, "is not a number, it is a", typeof arg);
+      if (typeof arg === "string") {
+        const newNum = Number(arg.toPrecision());
+        console.log(newNum);
+        this.setState({
+          strLength: newNum
+        });
+        console.log(typeof this.state.strLength);
+      }
+    } else console.log("it's a number");
+  };
 
   componentDidMount() {
     this.start();
@@ -25,11 +41,12 @@ class ButtonStart extends React.Component {
   }
 
   start() {
+    // convert from to number
+    this.checktype(this.state.strLength);
     // get Bitstring with length strLength
     fetch(`http://localhost:8080/rest/randomstring/${this.state.strLength}`)
       .then(res => res.json())
       .then(data => {
-        console.log(data);
         this.setState({ bitString: data.bitString });
         this.props.updateProps({
           bitString: this.state.bitString
@@ -40,7 +57,6 @@ class ButtonStart extends React.Component {
     fetch(`http://localhost:8080/rest/randombase/${this.state.strLength}`)
       .then(res => res.json())
       .then(data => {
-        console.log(data);
         this.setState({ baseString: data.baseString });
         this.props.updateProps({
           baseString: this.state.baseString
@@ -50,9 +66,16 @@ class ButtonStart extends React.Component {
   }
 
   render() {
+    const buttonStyle = {
+      padding: "5px",
+      border: "1px solid #999",
+      borderRadius: "5px",
+      font: "32px"
+    };
+
     return (
       <div>
-        <button onClick={this.start.bind(this)}>
+        <button onClick={this.start.bind(this)} style={buttonStyle}>
           Start, Length: {this.state.strLength}
         </button>
       </div>
