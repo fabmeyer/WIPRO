d788527d3816e300cc43f58aec77cad2edecf02d
fabmeyer <fabian@meyerfoto.ch>
2019-10-22 15:02:36 +0200

    get base and render base

--- wipro-v1/components/button-start.js
+++ wipro-v1/components/button-start.js
@@ -6,8 +6,13 @@ class ButtonStart extends React.Component {
 
     this.state = {
       strLength: this.props.strLength,
-      bitString: null
+      bitString: this.props.bitString,
+      baseString: this.props.baseString
     };
+  }
+
+  componentDidMount() {
+    this.start();
   }
 
   componentDidUpdate(oldProps) {
@@ -20,26 +25,36 @@ class ButtonStart extends React.Component {
   }
 
   start() {
-    // TODO: insert start logic here
+    // get Bitstring with length strLength
     fetch(`http://localhost:8080/rest/randomstring/${this.state.strLength}`)
       .then(res => res.json())
       .then(data => {
         console.log(data);
         this.setState({ bitString: data.bitString });
+        this.props.updateProps({
+          bitString: this.state.bitString
+        });
       })
       .catch(console.log);
-    this.props.updateProps({
-      bitString: this.state.bitString
-    });
+    // get Basestring with length strLength
+    fetch(`http://localhost:8080/rest/randombase/${this.state.strLength}`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        this.setState({ baseString: data.baseString });
+        this.props.updateProps({
+          baseString: this.state.baseString
+        });
+      })
+      .catch(console.log);
   }
 
   render() {
     return (
       <div>
-        <button onClick={this.start.bind(this, this.state.strLength)}>
+        <button onClick={this.start.bind(this)}>
           Start, Length: {this.state.strLength}
         </button>
-        {/* <p>Bitstring is: {this.state.bitString}</p> */}
       </div>
     );
   }
