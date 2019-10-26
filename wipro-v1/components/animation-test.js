25784cd4381f83f1d642d290b42a34912720a566
fabmeyer <fabian@meyerfoto.ch>
2019-10-26 21:12:02 +0200

    added small animation, buggy on Chrome, working on Safari

--- wipro-v1/components/animation-test.js
+++ wipro-v1/components/animation-test.js
@@ -1,18 +1,27 @@
 const React = require("react");
-import ReactRough, { Rectangle, Circle } from "react-rough";
+import ReactRough, { Circle } from "react-rough";
+import Anime from "react-anime";
 
 class AnimationTest extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = {};
   }
 
   render() {
     return (
-      <ReactRough width={200} height={400}>
-        <Circle points={[100, 100, 100]} fill="yellow" fillWeight="1" />
-      </ReactRough>
+      <Anime
+        easing="easeInSine"
+        duration={2000}
+        direction="normal"
+        loop={true}
+        translateX="100%"
+      >
+        <div>
+          <ReactRough width={200} height={200}>
+            <Circle points={[100, 100, 100]} fill="yellow" fillWeight="1" />
+          </ReactRough>
+        </div>
+      </Anime>
     );
   }
 }
