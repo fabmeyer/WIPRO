91fca9dc6525943eb5881f1a9961119d6694fb90
fabmeyer <fabian@meyerfoto.ch>
2019-10-27 21:58:47 +0100

    animation 2

--- wipro-v1/components/animation-test.js
+++ wipro-v1/components/animation-test.js
@@ -5,51 +5,40 @@ import AnimationCircle from "./animation-circle";
 class AnimationTest extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      shift: [100, 200, 300]
-    };
+    this.state = {};
   }
-
-  componentDidMount() {
-    console.log("component did mount");
-    this.interval = setInterval(() => {
-      console.log(this.state.shift);
-      this.setState(state => {
-        const shift = state.shift.map(c => c + 100);
-        // return { shift };
-      });
-    }, 1000);
-  }
-
-  componentWillUnmount = () => {
-    console.log("component will unmount");
-    clearInterval(this.interval);
-  };
 
   render() {
     const animationContainer = {
       position: "absolute"
     };
     return (
-      <Anime
-        easing="linear"
-        elasticity="0"
-        duration={2000}
-        direction="normal"
-        loop={true}
-        delay={(el, index) => index * 1000}
-        translateX={(el, index) => 200}
-      >
-        <span style={animationContainer}>
-          <AnimationCircle color="yellow" x={100} y={100}></AnimationCircle>
-        </span>
-        <span style={animationContainer}>
-          <AnimationCircle color="orange" x={300} y={100}></AnimationCircle>
-        </span>
-        <span style={animationContainer}>
-          <AnimationCircle color="red" x={500} y={100}></AnimationCircle>
-        </span>
-      </Anime>
+      <React.Fragment>
+        <Anime
+          easing="easeInOutExpo"
+          elasticity="0.5"
+          duration={2000}
+          direction="normal"
+          loop={true}
+          translateX={300}
+        >
+          <span style={animationContainer}>
+            <AnimationCircle color="yellow" x={100} y={100}></AnimationCircle>
+          </span>
+        </Anime>
+        <Anime
+          easing="easeInOutExpo"
+          elasticity="0.5"
+          duration={2000}
+          direction="normal"
+          loop={true}
+          translateX={300}
+        >
+          <span style={animationContainer}>
+            <AnimationCircle color="yellow" x={400} y={100}></AnimationCircle>
+          </span>
+        </Anime>
+      </React.Fragment>
     );
   }
 }
